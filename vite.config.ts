ee9bfefa2d781bf2787dc8d1c51d7293127b86b7
Your Name <your.email@example.com>
2025-09-27 23:00:51 +0530

    Update deployment configuration with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/ai_internship_recommendation_engine_backend/' : '/',
+  base: '/intern_backend/',
   plugins: [
     react(),
     // The code below enables dev tools like taking screenshots of your site
